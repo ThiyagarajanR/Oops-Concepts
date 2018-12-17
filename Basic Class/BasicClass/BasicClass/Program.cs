78e980072d26a2258b4abd1de96dc9f5c12bf2bb
Thiyagarajan <thiyagarajan168@gmail.com>
2018-12-17 11:33:09 +0530

    Update Program.cs

--- Basic Class/BasicClass/BasicClass/Program.cs
+++ Basic Class/BasicClass/BasicClass/Program.cs
@@ -6,7 +6,27 @@ namespace BasicClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creating Object for the basicClass
+
+            basicClass bs = new basicClass();
+
+            bs.MethodCall();  //Calling the method using basicClass object
+            
+        }
+    }
+
+    public class basicClass
+    {
+        public basicClass()
+        {
+            Console.WriteLine("Basic Class Constructor is called! \n The Object has been created for the class BasicClass");
+        }
+
+        public void MethodCall()
+        {
+
+            Console.WriteLine("Method is called");
+
         }
     }
 }
