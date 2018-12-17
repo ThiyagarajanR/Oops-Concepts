1badc9fa83ed084b1d3575d9bb25b7304d5f1712
Thiyagarajan <thiyagarajan168@gmail.com>
2018-12-17 11:21:06 +0530

    basic class created
    Class created

---
+++ Basic Class/BasicClass/BasicClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
